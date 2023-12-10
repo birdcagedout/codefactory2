ce28c2a5894442e5609d875e634dad0dfb3759bc
birdcagedout <birdcagedout@gmail.com>
2023-12-11 00:05:28 +0900

    20231211_home

--- p01_webview/lib/screen/home_screen.dart
+++ p01_webview/lib/screen/home_screen.dart
@@ -4,8 +4,13 @@ import 'package:webview_flutter/webview_flutter.dart';
 final homeUrl = Uri.parse('https://blog.codefactory.ai');
 
 class HomeScreen extends StatelessWidget {
-  WebViewController controller = WebViewController()..loadRequest(homeUrl);
-  HomeScreen({super.key});
+  // WebViewController controller = WebViewController()..loadRequest(homeUrl);
+  WebViewController controller = WebViewController();
+
+  HomeScreen({super.key}) {
+    controller.loadRequest(homeUrl);
+    controller.setJavaScriptMode(JavaScriptMode.unrestricted);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +19,16 @@ class HomeScreen extends StatelessWidget {
         title: Text("코드팩토리"),
         centerTitle: true,
         backgroundColor: Colors.orange,
+        actions: [
+          IconButton(
+            onPressed: () {
+              print("Pressed");
+              controller.loadRequest(homeUrl);
+            },
+            icon: Icon(Icons.home),
+            iconSize: 30,
+          ),
+        ],
       ),
       body: WebViewWidget(
         controller: controller,
