3f58930de77b84e175f575bba1fb5f40fa0b0e7f
birdcagedout <birdcagedout@gmail.com>
2023-12-11 22:41:27 +0900

    20231211_work

--- p01_webview/lib/screen/home_screen.dart
+++ p01_webview/lib/screen/home_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-final homeUrl = Uri.parse('https://blog.codefactory.ai');
+// http 사이트에 접속하는 방법 (기본 세팅은 https 연결만 가능)
+// 1. iOS     : ios/Runner/info.plist 맨 끝 부분 <key>NSAppTransportSecurity</key> ~ </dict>까지 7줄 추가
+// 2. Android : android/app/src/main/androidManifest.xml 2번째 줄(manifest 끝난 바로 다음줄), 7번째 줄(application 요소의 마지막 속성에 추가)
+// final homeUrl = Uri.parse('https://blog.codefactory.ai');
+final homeUrl = Uri.parse('http://www.ktword.co.kr');
 
 class HomeScreen extends StatelessWidget {
+  // cascade operator : WebViewController 인스턴스를 생성하고 loadRequest()를 실행하되, 인스턴스를 반환한다
   // WebViewController controller = WebViewController()..loadRequest(homeUrl);
   WebViewController controller = WebViewController();
 
@@ -27,6 +32,7 @@ class HomeScreen extends StatelessWidget {
             },
             icon: Icon(Icons.home),
             iconSize: 30,
+            color: Colors.white,
           ),
         ],
       ),
