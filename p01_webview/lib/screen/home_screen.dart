33dd42b6dea52006322b2d113888e3ec46f30445
birdcagedout <birdcagedout@gmail.com>
2023-12-13 19:05:49 +0900

    20231213_work

--- p01_webview/lib/screen/home_screen.dart
+++ p01_webview/lib/screen/home_screen.dart
@@ -4,8 +4,8 @@ import 'package:webview_flutter/webview_flutter.dart';
 // http 사이트에 접속하는 방법 (기본 세팅은 https 연결만 가능)
 // 1. iOS     : ios/Runner/info.plist 맨 끝 부분 <key>NSAppTransportSecurity</key> ~ </dict>까지 7줄 추가
 // 2. Android : android/app/src/main/androidManifest.xml 2번째 줄(manifest 끝난 바로 다음줄), 7번째 줄(application 요소의 마지막 속성에 추가)
-// final homeUrl = Uri.parse('https://blog.codefactory.ai');
-final homeUrl = Uri.parse('http://www.ktword.co.kr');
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+// final homeUrl = Uri.parse('http://www.ktword.co.kr');
 
 class HomeScreen extends StatelessWidget {
   // cascade operator : WebViewController 인스턴스를 생성하고 loadRequest()를 실행하되, 인스턴스를 반환한다
---
+++ p02_row_column/lib/screen/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          color: Colors.black,
+          height: 100,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.red,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.orange,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
